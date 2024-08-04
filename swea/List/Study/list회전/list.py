383e45ff385a546e751b52ea9be17473e59d3ed9
mins <m2insapp@gmail.com>
2024-08-04 22:58:16 +0900

    rotate

---
+++ swea/List/Study/list회전/list.py
@@ -0,0 +1,26 @@
+
+
+
+def rotate_90(matrix):
+
+    transpose = list(zip(*matrix))
+    for row in transpose:
+        print(row)
+    rotate = [list(row)[::-1] for row in transpose]
+    return rotate
+
+
+
+array = [[1,2,3], [4,5,6], [7,8,9]]
+
+for row in array:
+    print(row)
+
+print()
+rotate_arr = rotate_90(array)
+
+
+print()
+
+for row in rotate_arr:
+    print(row)
