809e54de28d0bc510be7465d436b408da6b9bb30
mins <m2insapp@gmail.com>
2024-08-12 17:39:05 +0900

    baek

--- study/IM/baek/2628_종이자르기/2628.py
+++ study/IM/baek/2628_종이자르기/2628.py
@@ -5,9 +5,6 @@ sys.stdin = open('input.txt')
 
 # Testcase 수
 
-
-    # 가로,세로방향 1cm마다 점선이 그어져있다.
-
 # 위 > 아래 1번부터 번호가 붙여있고 왼 > 오
 
 # 가로 세로길이, 잘라야할 점선들. 가장큰 종이 조각 몇cm2?
@@ -16,8 +13,12 @@ sys.stdin = open('input.txt')
 row, col = map(int,input().split())
 cut_count = int(input())
 cut_list = [list(map(int, input().split())) for _ in range(cut_count)] #[[0, 3], [1, 4], [0, 2]]
+matrix = [[0] * row for _ in range(col)]
 
-
-# for i in range(cut_count):
-#가로로 자르는 점선은 0과 전선 번호
-#세로는 1과 점선번호
+# 0은 가로로자르기
+# 1은 세로로자르기
+result = 0
+result_arr = []
+# 가장 큰 넓이 추출.
+for r, c in cut_list:
+    # 만약 가로라면
