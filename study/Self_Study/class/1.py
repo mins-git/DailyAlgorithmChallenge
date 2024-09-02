7706a84250f9e96a794be6161cf6964b60ba438c
mins <m2insapp@gmail.com>
2024-09-02 23:07:44 +0900

    baek 1932

--- study/Self_Study/class/1.py
+++ study/Self_Study/class/1.py
@@ -1,5 +1,23 @@
+import sys
+sys.stdin = open('input.txt')
 
 """
-트리 정점의 총수 V 간선
+전위순회로 해야됨.
+트리 정점의 총수 V 간선 N'
 
 """
+
+# 중위 순회 => 왼쪽 -> 자신 -> 오른쪽
+def inorder(node):
+    # 자식이 없는 경우
+    if not tree[node]:
+        return
+    # 왼쪽에 자식이 있는지 확인
+    inorder(int(tree[node][1]))
+    # 문자 출력
+    print(tree[node][0], end="")
+    # 오른쪽에 자식이 있는지 확인
+    inorder(int(tree[node][2]))
+
+
+
