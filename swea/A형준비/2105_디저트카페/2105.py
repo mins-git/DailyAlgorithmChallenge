beb4b2de995eb2a74deea780c16fcaf891faabc7
min <m2insapp@gmail.com>
2024-09-09 11:37:18 +0900

    feat: 숫자 만들기

--- swea/A형준비/2105_디저트카페/2105.py
+++ swea/A형준비/2105_디저트카페/2105.py
@@ -10,41 +10,80 @@ sys.stdin = open('input.txt')
 - 디저트 최대한 많이 먹고싶음.
 - 디저트 못먹으면 -1
 """
+#
+# direction = [(1, 1), (1, -1), (-1, -1), (-1, 1)]  # 시계방향 기준으로 12시 - 3시 / 3시 -6시 / 6시 - 9시 / 9시 - 12시
+#
+#
+# def dessert_ju_se_yo(i,j, start_i, start_j, visited,N, cafe):
+#
+#     if i == start_i and j == start_j and len(visited) > 0:
+#         return len(visited)
+#
+#     max_length = 0
+#
+#     # 방향 변경시 필요한 코드
+#     for k in range(4):
+#
+#         nr = i + direction[k][0]
+#         nc = j + direction[k][1]
+#         if 0 <= nr < N and 0 <= nc < N and (nr,nc) not in visited:
+#             if cafe[nr][nc] not in visited:
+#                 visited.add(cafe[nr][nc])
+#                 max_length = max(max_length, dessert_ju_se_yo(nr,nc, start_i,start_j,visited,N,cafe))
+#                 visited.remove(cafe[nr][nc])
+#     return max_length
+#
+# T = int(input())
+#
+# for tc in range(1, T+1):
+#     N = int(input())
+#     cafe = [list(map(int, input().split())) for _ in range(N)]
+#     max_dessert = -1
+#
+#     for i in range(N):
+#         for j in range(N):
+#             visited = set()
+#             visited.add(cafe[i][j])
+#             max_dessert = max(max_dessert, dessert_ju_se_yo(i,j, i, j, visited, N, cafe))
+#
+#
+#
+#     print(f"#{tc} {max_dessert}")
 
-direction = [(1, -1), (-1, -1), (-1, 1), (1, 1)] # 시계방향 기준으로 12시 - 3시 / 3시 -6시 / 6시 - 9시 / 9시 - 12시
 
+def explore(x, y, start_x, start_y, direction, lst):
+    if direction == 4:
+        return
 
-def dessert_ju_se_yo(i,j, visited):
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if (nx, ny) == (start_x, start_y) and len(lst) >= 4:  # 사각형이 만들어 지면
+        route.append(lst[:])
+        return
 
-    visited = [[False] * N for _ in range(N)]
-
-    visited[i, j] = True
-
-    # 방향 변경시 필요한 코드
-    for k in range(4):
-
-        nr = i + direction[k][0]
-        nc = j + direction[k][1]
-        arr = [cafe[i][j]]
-
-        while 0 <= nr <= N and 0 <= nc < N: # 행렬안에 있고,
-            if cafe[nr][nc] not in arr and # arr 배열에 없고,
-    # if k + 1 >= 4:
-    #     k = 0
-    # else:
-    #     k += 1
+    if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] not in lst:
+        lst.append(arr[nx][ny])
+        explore(nx, ny, start_x, start_y, direction, lst)
+        explore(nx, ny, start_x, start_y, direction + 1, lst)
+        lst.pop()
 
 
 T = int(input())
-
+# Testcase 만큼 반복
 for tc in range(1, T+1):
-    N = int(input())
-    cafe = [list(map(int, input().split())) for _ in range(N)]
-    recent = []
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
 
-    for i in range(N):
-        for j in range(N):
-            k = dessert_ju_se_yo(i, j)
+    dx = [1, 1, -1, -1] # 시계방향
+    dy = [1, -1, -1, 1]
+    route = []
 
+    for i in range(n):
+        for j in range(1,n-1):
+            explore(i, j, i, j, 0, [arr[i][j]])
 
-
+    if len(route) > 0:
+        answer = max(len(i) for i in route)
+        print(f'#{tc} {answer}')
+    else:
+        print(f'#{tc} -1')
