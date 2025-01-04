161d409e1ee7e3bbee2ebf984bf59985815e1dc5
mins <113077033+mins-git@users.noreply.github.com>
2025-01-04 23:45:03 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 172 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
@@ -1,8 +1,9 @@
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main{
     static int[][] delt = {
@@ -12,6 +13,7 @@ public class Main{
             {-1, 0}   // 위 (세로 -1)
     };
 
+
     public static int DFS(int j, int o, ArrayList<ArrayList<String>> mapList, boolean[][] visited, boolean mySold ){
 
         visited[j][o] = true;
@@ -19,11 +21,10 @@ public class Main{
         int power = 1; // 현재 병사의 시작은 1
 
         // 네 방향으로 탐색
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i< 4; i++){
             int nj = j + delt[i][0];
             int no = o + delt[i][1];
 
-            // 인덱스 범위 체크: 0 <= nj < M, 0 <= no < N
             if(nj >= 0 && nj < mapList.size() && no >= 0 && no < mapList.get(0).size()){
 
                 // 방문하지 않았고, 같은 팀 병사라면 DFS 진행
@@ -36,17 +37,17 @@ public class Main{
     }
 
     public static void main(String[] args) throws IOException {
-//        String input = "src/baekJoon/input.txt";
+//      String input = "src/baekJoon/input.txt";
 //        BufferedReader br = new BufferedReader(new FileReader(input));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String [] inputX = br.readLine().split(" ");
 
-        int N = Integer.parseInt(inputX[0]); // 세로
-        int M = Integer.parseInt(inputX[1]); // 가로
+        int N = Integer.parseInt(inputX[0]); // 가로
+        int M = Integer.parseInt(inputX[1]); // 세로
 
         ArrayList<ArrayList<String>> mapList = new ArrayList<>();
 
-        for(int i = 0; i < M; i++){  // i는 0부터 시작, 가로 방향
+        for(int i = 0; i < M; i++){
             ArrayList<String> row = new ArrayList<>();
             String[] line = br.readLine().split(""); // 입력을 한 글자씩 분리
 
@@ -56,18 +57,21 @@ public class Main{
             mapList.add(row); // 한 줄을 mapList에 추가
         }
 
-        // 방문 처리할 2차원 배열 (M x N 크기)
+//        System.out.println(mapList);
+
+
+        // 방문 처리할 2차원 배열
         boolean[][] visited = new boolean[M][N];
         int totalWPower = 0;
         int totalBPower = 0;
 
         // 가로 체크
-        for (int o = 0; o < N; o++){
+        for (int o = 0; o < N; o ++){
             // 세로 체크
             for(int j = 0; j < M; j++){
                 if(!visited[j][o]){
 
-                    // 흰옷과 파란옷에 따라 dfs 진행
+                    // 흰옷과  파란옷에 따라 dfs 진행
                     if (mapList.get(j).get(o).equals("W")){
                         totalWPower += Math.pow(DFS(j, o, mapList, visited, true), 2); // 흰옷 병사의 위력 제곱 합
                     } else {
@@ -78,5 +82,6 @@ public class Main{
         }
 
         System.out.println(totalWPower + " " + totalBPower);
+
     }
 }
